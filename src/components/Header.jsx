18288bb7adebc01e18c04c798750aa6993187197
Barden777 <105597467+Barden777@users.noreply.github.com>
2022-10-28 17:41:56 +0300

    Add files via upload
    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SendIcon from "@mui/icons-material/Send";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <SendIcon />
+        <span> </span>
+        Quick NOTES
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
